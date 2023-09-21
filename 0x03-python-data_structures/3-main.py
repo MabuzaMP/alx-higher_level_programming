a9000a279e26d8bf5c0f7d34d7cec23a7e49e487
Mpumelelo Mabuza <132443084+MabuzaMP@users.noreply.github.com>
2023-09-21 12:45:03 +0200

    correction 3-main.py 2

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-print_reversed_list_integer =\
-__import__('3-print_reversed_list_integer').print_reversed_list_integer
 
+def print_reversed_list_integer(my_list=[]):
+   for i in range(len(my_list) - 1, -1, -1):
+    print(str.format("%d" % my_list[i]))
 
-print_reversed_list_integer([1, 2, 3, 4, 5])
+  
+>>> print_reversed_list_integer([1, 2, 3, 4, 5])
